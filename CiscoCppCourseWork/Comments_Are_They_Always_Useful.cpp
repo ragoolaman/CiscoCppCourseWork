9980d3a9a69a2441a4cfe3b5ea0c73be3c8ba9d3
ragoolaman <ragoolaman01@gmail.com>
2017-02-05 16:33:19 -0700

    Add project files.

---
+++ CiscoCppCourseWork/Comments_Are_They_Always_Useful.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+int main()
+{
+	int v = 10800; // 3*60*60
+				   //hold time values in seconds:
+	int zzz = 3 * 60;
+	zzz = 5 * 60;
+	float siii = 3.141526; //it's the value of pi
+	//std::cout << "result: " << result << std::endl;
+}
+	
